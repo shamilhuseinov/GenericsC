a44d07cdc20c169e63ad399189fb7d7138bea508
shamilhuseinov <shamilrh@code.edu.az>
2023-02-07 19:57:27 +0400

    homework is added

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace test
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //1
+            void GenericMethod<T>(T param)
+            {
+                Console.WriteLine(param);
+            }
+
+            GenericMethod<int>(7);
+
+            //2
+            void GenericMethod2<T>(T param1, T param2)
+            {
+                T temp = param1;
+                param1 = param2;
+                param2 = temp;
+                Console.WriteLine($"{param1}, {param2}");
+            }
+
+            GenericMethod2<string>("salam", "sagol");
+        }
+    }
+}
+
